2a79326c5e8b9b21826d274f91ea2bb1f0803071
Miguel Torre <m.a.torre.f@gmail.com>
2022-04-19 09:42:53 -0700

    maybe starting with the miniproject as canvas wasnt such a great idea

---
+++ controllers/ThoughtController.js
@@ -0,0 +1,56 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+  // Get all courses
+  getAllThoughts(req, res) {
+    Thought.find()
+      .then((courses) => res.json(courses))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a course
+  getThoughtsById(req, res) {
+    Thought.findOne({ _id: req.params.courseId })
+      .select('-__v')
+      .then((course) =>
+        !course
+          ? res.status(404).json({ message: 'No thoughts with that ID' })
+          : res.json(course)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a course
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((course) => res.json(course))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Delete a course
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.courseId })
+      .then((course) =>
+        !course
+          ? res.status(404).json({ message: 'No course with that ID' })
+          : Student.deleteMany({ _id: { $in: course.students } })
+      )
+      .then(() => res.json({ message: 'Course and students deleted!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a course
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.courseId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((course) =>
+        !course
+          ? res.status(404).json({ message: 'No course with this id!' })
+          : res.json(course)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
+//Need to add reactions
